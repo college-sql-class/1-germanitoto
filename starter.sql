a801489fa97917840d5ac51dee60f0d8bdd7c8b9
germanitoto <germansmolnikov2006@gmail.com>
2025-10-21 20:58:53 +0300

    Практика 1 выполнена

--- starter.sql
+++ starter.sql
@@ -1,7 +1,6 @@
--- В этом файле вы пишете свои SQL-запросы
--- Например:
--- UPDATE где SET что WHERE условие;
--- DELETE FROM где WHERE условие;
-
+-- In this file you write your SQL queries
+-- For example:
+-- UPDATE table SET column WHERE condition;
+-- DELETE FROM table WHERE condition;
 UPDATE students SET name = 'Alice_updated', age = 22 WHERE id = 1;
 DELETE FROM students WHERE id = 2;
